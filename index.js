0879b005b6cc8fc12053d5b09bf2c1c9434cd7d4
Stelian Rosca <stelianro>
2023-04-10 18:22:17 +0300

    getTeamsRequest

---
+++ index.js
@@ -0,0 +1,27 @@
+function getTeamsRequest() {
+    return fetch("http://localhost:3000/teams-json", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((r) => {
+        return r.json();
+    });
+}
+
+function getTeamAsHTML(team) {
+    return `<tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>${team.url}</td>
+    </tr>`;
+}
+function showTemas(teams) {
+    const html = teams.map(getTeamAsHTML);
+    document.querySelector("table tbody").innerHTML = html.join("");
+}
+
+getTeamsRequest().then((teams) => {
+    showTemas(teams);
+});
