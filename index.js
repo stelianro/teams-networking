90480a5d934f2d46d0f75a801799b03192c04c62
Stelian Rosca <stelianro>
2023-04-10 20:03:27 +0300

    createTeamRequest

--- index.js
+++ index.js
@@ -9,19 +9,62 @@ function getTeamsRequest() {
     });
 }
 
+function createTeamsRequest(team) {
+    return fetch("http://localhost:3000/teams-json/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(team),
+    }).then((r) => r.json());
+}
+
 function getTeamAsHTML(team) {
     return `<tr>
         <td>${team.promotion}</td>
         <td>${team.members}</td>
         <td>${team.name}</td>
         <td>${team.url}</td>
+        <td></td>
     </tr>`;
 }
 function showTemas(teams) {
     const html = teams.map(getTeamAsHTML);
-    document.querySelector("table tbody").innerHTML = html.join("");
+    $("table tbody").innerHTML = html.join("");
+}
+
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+    console.warn(e);
+
+    const promotion = $("#promotion").value;
+    const members = $("#members").value;
+    const name = $("#name").value;
+    const url = $("#url").value;
+
+    const team = {
+        promotion: promotion,
+        members: members,
+        name: name,
+        url: url,
+    };
+
+    createTeamsRequest(team).then((status) => {
+        console.info("status", status);
+        window.location.reload();
+    });
+}
+
+function initEvents() {
+    $("#editForm").addEventListener("submit", formSubmit);
 }
 
 getTeamsRequest().then((teams) => {
     showTemas(teams);
 });
+
+initEvents();
