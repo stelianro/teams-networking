f5a57fe7e8d695df48277ba692c90ec187459e67
Stelian Rosca <stelianro>
2023-04-10 20:33:45 +0300

    deleteTeamRequest

--- index.js
+++ index.js
@@ -19,13 +19,25 @@ function createTeamsRequest(team) {
     }).then((r) => r.json());
 }
 
+function deleteTeamRequest(id) {
+    return fetch("http://localhost:3000/teams-json/delete", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+    }).then((r) => r.json());
+}
+
 function getTeamAsHTML(team) {
     return `<tr>
         <td>${team.promotion}</td>
         <td>${team.members}</td>
         <td>${team.name}</td>
         <td>${team.url}</td>
-        <td></td>
+        <td>
+            <a data-id="${team.id}">X</a>
+        </td>
     </tr>`;
 }
 function showTemas(teams) {
@@ -47,10 +59,10 @@ function formSubmit(e) {
     const url = $("#url").value;
 
     const team = {
-        promotion: promotion,
-        members: members,
-        name: name,
-        url: url,
+        promotion,
+        members,
+        name,
+        url,
     };
 
     createTeamsRequest(team).then((status) => {
@@ -59,8 +71,24 @@ function formSubmit(e) {
     });
 }
 
+function deleteTeam(id) {
+    console.warn("delete", id);
+    deleteTeamRequest(id).then((status) => {
+        console.warn("status", status);
+        if (status.success) {
+            window.location.reload();
+        }
+    });
+}
+
 function initEvents() {
     $("#editForm").addEventListener("submit", formSubmit);
+    $("table tbody").addEventListener("click", (e) => {
+        if (e.target.matches("a")) {
+            const id = e.target.dataset.id;
+            deleteTeam(id);
+        }
+    });
 }
 
 getTeamsRequest().then((teams) => {
